fe0ec7891dc506db0e26ad11ff20009d5b6197b7
Omar Balbaa <69731891+omarbalbaa@users.noreply.github.com>
2024-05-23 16:51:14 +0300

    connected to MongoDB and replaced simple array data

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -1,47 +1,59 @@
-let { courses } = require("../data/courses");
 const { validationResult } = require("express-validator");
 
-const getAllCourses = (req, res) => {
+const Course = require("../models/course.model");
+
+const getAllCourses = async (req, res) => {
+  const courses = await Course.find();
   res.json(courses);
 };
 
-const getSingleCourse = (req, res) => {
-  const courseId = +req.params.courseId;
-  const course = courses.find((course) => course.id === courseId);
-  if (!course) {
-    return res.status(404).json({ msg: "course does not exist" });
+const getSingleCourse = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.courseId);
+    if (!course) {
+      return res.status(404).json({ msg: "course does not exist" });
+    }
+    return res.json(course);
+  } catch (err) {
+    return res.status(400).json({ msg: "invalid object ID" });
   }
-  res.json(course);
 };
 
-const addCourse = (req, res) => {
+const addCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  const course = { id: courses.length + 1, ...req.body };
-  courses.push(course);
-  res.status(201).json(course);
+  const newCourse = new Course(req.body);
+  await newCourse.save();
+
+  // const course = { id: courses.length + 1, ...req.body };
+  // courses.push(course);
+  res.status(201).json(newCourse);
 };
 
-const updateCourse = (req, res) => {
-  const courseId = +req.params.courseId;
-
-  let course = courses.find((course) => course.id === courseId);
-
-  if (!course) {
-    return res.status(404).json({ msg: "course not found" });
+const updateCourse = async (req, res) => {
+  const courseId = req.params.courseId;
+  try {
+    const updatedCourse = await Course.updateOne({_id: courseId}, {
+      $set: { ...req.body }
+    });
+    return res.status(200).json(updatedCourse);
+  } catch (err) {
+    return res.status(400).json({ error: err });
   }
-
-  course = { ...course, ...req.body };
-  res.status(200).json(course);
 };
 
-const deleteCourse = (req, res) => {
-  const courseId = +req.params.courseId;
-  courses = courses.filter((course) => course.id !== courseId);
-  res.status(200).json({ success: true });
+const deleteCourse = async (req, res) => {
+  const courseId = req.params.courseId;
+  try {
+    const deleteCourse = await Course.deleteOne({_id: courseId});
+    res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({error: err});
+  } 
+  
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://balbaa:codeZone@cluster0.halviqt.mongodb.net/codeZone"
+mongoose.connect(url).then (()=> {
+  console.log('mongodb server started')
+})
+
 app.use(express.json());
 
 const coursesRouter = require("./routes/courses.route");
